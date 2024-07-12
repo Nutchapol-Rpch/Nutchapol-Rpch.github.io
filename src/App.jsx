f5e41c63e58847b94a57bc458c6a180ce7646321
Smiffeed <97792035+Smiffeed@users.noreply.github.com>
2024-07-13 00:26:49 +0700

    new file:   assets/index-BPFfTv8F.js 	new file:   dist/assets/index-BPFfTv8F.js 	deleted:    dist/assets/index-Cyejp63o.js 	modified:   dist/index.html 	modified:   index.html 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,37 @@
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import './App.css'
 
 function App() {
 
   const [price, setPrice] = useState(0);
   const [discount, setDiscount] = useState(0);
-  const [vat, setVat] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [vatRate, setVatRate] = useState(0.07); // Example: 7% VAT rate
 
-  useEffect(() => {
-    const calculateTotals = () => {
-      const discountedPrice = price - discount;
-      const v = discountedPrice * 0.07;
-      setVat(v.toFixed(2));
-      const total = discountedPrice + v;
-      setTotal(total.toFixed(2));
-    };
-    calculateTotals();
-  }, [price, discount]);
+  const discountedPrice = price - price * discount;
 
-  function handle(e) {
-    let value = parseFloat(e.target.value) || 0;
-    if (e.target.name === "price") {
+  const vatAmount = discountedPrice * vatRate;
+
+  const grossPrice = discountedPrice + vatAmount;
+
+  const handle = (e) => {
+    const value = parseFloat(e.target.value) || 0;
+    if (e.target.name === 'price') {
       setPrice(value);
     } else {
       setDiscount(value);
     }
-  }
+  };
 
   return (
     <>
-      <p style={{fontSize: '30pt'}}>Price:
-        <input type="number" name="price" style={{fontSize: '30pt'}} onChange={handle} />
+      <p style={{ fontSize: '30pt' }}>Price:
+        <input type="number" name="price" style={{ fontSize: '30pt' }} onChange={handle} />
       </p>
-      <p style={{fontSize: '30pt'}}>Discount:
-        <input type="number" name="discount" style={{fontSize: '30pt'}} onChange={handle} />
+      <p style={{ fontSize: '30pt' }}>Discount:
+        <input type="number" name="discount" style={{ fontSize: '30pt' }} onChange={handle} />
       </p>
-      <h1>VAT = {vat}</h1>
-      <h1>Gross Price = {total}</h1>
+      <h1>VAT = {vatAmount.toFixed(2)}</h1>
+      <h1>Gross Price = {grossPrice.toFixed(2)}</h1>
     </>
   );
 }
